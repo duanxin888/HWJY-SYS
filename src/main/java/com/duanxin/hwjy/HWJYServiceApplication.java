fd6a859d3d2153aaf9ee80f5128e85d5e214bd90
dx134 <18870735026@163.com>
2021-01-08 18:45:01 +0800

    init push

---
+++ src/main/java/com/duanxin/hwjy/HWJYServiceApplication.java
@@ -0,0 +1,18 @@
+package com.duanxin.hwjy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author duanxin
+ * @version 1.0
+ * @className HWJYServiceApplication
+ * @date 2021/01/08 18:28
+ */
+@SpringBootApplication
+public class HWJYServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HWJYServiceApplication.class, args);
+    }
+}
