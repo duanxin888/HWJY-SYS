fd67bd35133afc805bc09787fdff02909550abb0
dx134 <18870735026@163.com>
2021-01-29 09:44:38 +0800

    add api of [/api/v1/users/tokens/refresh]

--- src/main/java/com/duanxin/hwjy/api/facade/UserApi.java
+++ src/main/java/com/duanxin/hwjy/api/facade/UserApi.java
@@ -4,13 +4,14 @@ import com.duanxin.hwjy.api.assembler.UserAssembler;
 import com.duanxin.hwjy.api.dto.LoginCommandDto;
 import com.duanxin.hwjy.application.service.command.UserAppService;
 import com.duanxin.hwjy.domain.user.entity.UserDO;
+import com.duanxin.hwjy.infrastructure.client.token.JwtClient;
 import com.duanxin.hwjy.infrastructure.common.api.ResponseResult;
+import com.duanxin.hwjy.infrastructure.common.exception.HWJYCheckException;
+import com.duanxin.hwjy.infrastructure.common.exception.ResultEnum;
 import lombok.AllArgsConstructor;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -27,11 +28,20 @@ import javax.validation.Valid;
 public class UserApi {
 
     private final UserAppService userAppService;
+    private final JwtClient jwtClient;
 
     @PostMapping("/login")
     public ResponseResult login(@RequestBody @Valid LoginCommandDto loginCommandDto) {
         UserDO userDO = UserAssembler.loginCommand2DO(loginCommandDto);
         userDO = userAppService.login(userDO, loginCommandDto.getCode(), loginCommandDto.getAppid());
         return ResponseResult.success(UserAssembler.do2LoginResponse(userDO, loginCommandDto));
+    }
+
+    @PutMapping("/tokens/refresh")
+    public ResponseResult refreshToken(@RequestBody String token) {
+        if (StringUtils.isBlank(token)) {
+            throw new HWJYCheckException(ResultEnum.USER_NOT_LOG_IN);
+        }
+        return ResponseResult.success(jwtClient.refreshToken(token));
     }
 }
