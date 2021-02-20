3d2ed5261faa83fa98bf16a2712aad2f82ea70c7
dx134 <18870735026@163.com>
2021-02-20 20:48:49 +0800

    add log & lock of CounterResetJob

--- src/main/java/com/duanxin/hwjy/api/job/CounterResetJob.java
+++ src/main/java/com/duanxin/hwjy/api/job/CounterResetJob.java
@@ -1,11 +1,16 @@
 package com.duanxin.hwjy.api.job;
 
 import com.duanxin.hwjy.infrastructure.client.counter.CounterClient;
+import com.duanxin.hwjy.infrastructure.client.lock.LockClient;
+import com.duanxin.hwjy.infrastructure.common.constants.Constants;
 import com.duanxin.hwjy.infrastructure.common.constants.CounterConstants;
+import com.duanxin.hwjy.infrastructure.common.constants.LockKeyConstants;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+
+import java.time.Duration;
 
 /**
  * @author duanxin
@@ -19,10 +24,18 @@ import org.springframework.stereotype.Component;
 public class CounterResetJob {
 
     private final CounterClient counterClient;
+    private final LockClient lockClient;
 
     @Scheduled(cron = "0 0 0 * * ? *")
     public void counterReset() {
+        log.info("begin to execute CounterResetJob");
         for (CounterConstants value : CounterConstants.values()) {
+            String key = LockKeyConstants.COUNTER_RESET_JOB_KEY.getKey()
+                    + Constants.CACHE_SEP.getDesc() + value.name();
+            if (!lockClient.lock(key, Duration.ofMinutes(30))) {
+                log.info("failed to fetch lock [{}]", key);
+                continue;
+            }
             counterClient.reset(value.name());
         }
     }
---
+++ src/main/java/com/duanxin/hwjy/infrastructure/common/constants/LockKeyConstants.java
@@ -0,0 +1,19 @@
+package com.duanxin.hwjy.infrastructure.common.constants;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+/**
+ * @author duanxin
+ * @version 1.0
+ * @className LockKeyConstants
+ * @date 2021/02/20 20:37
+ */
+@AllArgsConstructor
+@Getter
+public enum LockKeyConstants {
+
+    COUNTER_RESET_JOB_KEY("JOB_KEY:COUNTER_RESET");
+
+    private final String key;
+}
